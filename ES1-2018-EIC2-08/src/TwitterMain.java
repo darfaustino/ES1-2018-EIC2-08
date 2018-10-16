8987df3c235be7e2f77809d6bf9f2de664d4dea1
Goncalo Cruz <gmczo@iscte-iul.pt>
2018-10-17 00:26:08 +0100

    1 commit twitter

---
+++ ES1-2018-EIC2-08/src/TwitterMain.java
@@ -0,0 +1,55 @@
+
+
+
+import java.util.List;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public final class TwitterMain  {
+	public static void main(String[] args) {
+        try {
+        	
+        	ConfigurationBuilder cb = new ConfigurationBuilder();
+        	cb.setDebugEnabled(true)
+        	  .setOAuthConsumerKey("W1f0VvgWPfT8OBqVxvy4Mw")
+        	  .setOAuthConsumerSecret("zKH2yAtRyefwsgOO8h8Szc4kru68iEm95QmIG7svw")
+        	  .setOAuthAccessToken("36481851-VhzByC4f9MSsZES1QZQ4e4iBvA9bWGLyv9HKFpy7c")
+        	  .setOAuthAccessTokenSecret("OahDuXF2Lhl5xlNYALhYZir6xSflAxKP9Zh89T05po");
+        	
+        	TwitterFactory tf = new TwitterFactory(cb.build());
+        	Twitter twitter = tf.getInstance();  
+        	getTimeline(twitter);
+           
+        } catch (Exception e) { System.out.println(e.getMessage()); }
+     }
+	
+	public static void getTimeline(Twitter twitter) throws TwitterException {
+	    List<Status> statuses = twitter.getHomeTimeline();
+	    System.out.println("Showing home timeline.");
+	    for (Status status : statuses) {
+	        System.out.println(status.getUser().getName() + ":" + status.getText());
+	        System.out.println("-------------------------");
+	    }
+	}
+	
+	public void search() throws TwitterException {
+	    Twitter twitter = TwitterFactory.getSingleton();
+	    Query query = new Query("source:twitter4j yusukey");
+	    QueryResult result = twitter.search(query);
+	    for (Status status : result.getTweets()) {
+	        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+	    }
+	}
+	
+	public static void post(Twitter twitter,String latestStatus) throws TwitterException {
+	    Status status = twitter.updateStatus(latestStatus);
+	    System.out.println("Successfully updated the status to [" + status.getText() + "].");
+	}
+}
+    
