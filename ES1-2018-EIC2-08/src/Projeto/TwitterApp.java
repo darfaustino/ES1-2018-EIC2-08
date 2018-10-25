52ad9048b84a9bdc8119297c2a54b03004489e43
gonca <gonca@DESKTOP-H4IK6O5.lan>
2018-10-25 22:28:03 +0100

    JUnits Twitter (InitTwitter, searchForUser, GetTimeLine)

---
+++ ES1-2018-EIC2-08/src/Projeto/TwitterApp.java
@@ -0,0 +1,112 @@
+package Projeto;
+/**Represents the twitter app
+ * 
+ * 
+ * @author  Gonçalo Cruz
+ * 
+ */
+
+
+import java.util.List;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public final class TwitterApp {
+
+	/**
+	 * Represents all posts in this page
+	 */
+	public static List<Status> statuses;
+	
+	
+	/** Creates a TwitterFactory with all the informations we have about the tokens that we gave
+	 * 
+	 * 
+	 */
+
+	public TwitterApp() {
+		
+		try {
+
+			ConfigurationBuilder cb = new ConfigurationBuilder();
+			cb.setDebugEnabled(true).setOAuthConsumerKey("W1f0VvgWPfT8OBqVxvy4Mw")
+					.setOAuthConsumerSecret("zKH2yAtRyefwsgOO8h8Szc4kru68iEm95QmIG7svw")
+					.setOAuthAccessToken("36481851-VhzByC4f9MSsZES1QZQ4e4iBvA9bWGLyv9HKFpy7c")
+					.setOAuthAccessTokenSecret("OahDuXF2Lhl5xlNYALhYZir6xSflAxKP9Zh89T05po");
+
+			TwitterFactory tf = new TwitterFactory(cb.build());
+			Twitter twitter = tf.getInstance();
+			
+			statuses = twitter.getHomeTimeline();
+			
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	
+	
+	/** List of latest tweets from user's home timeline.
+	 * 
+	 * 
+	 */
+	public static void getTimeline() throws TwitterException {
+		System.out.println("Showing home timeline.");
+		for (Status status : statuses) {
+			System.out.println(status.getUser().getName() + ":" + status.getText());
+			System.out.println("-------------------------");
+		}
+	}
+	
+
+	/** Searching by user's name.
+	 * 
+	 * @param s is the name of the user
+	 * 
+	 */
+	public static void searchForUser(String s) {
+		int counter = 0;
+		int counterTotal = 0;
+		for (Status status : statuses) {
+			if (status.getUser().getName() != null && status.getUser().getName().contains(s)) {
+				System.out.println(status.getUser().getName() + ":" + status.getText());
+				counter++;
+			}
+			counterTotal++;
+		}
+		System.out.println("-------------\nNº of Results: " + counter + "/" + counterTotal);
+
+	}
+
+//	/** Update "What are you doing?" via Twitter.updateStatus() method.
+//	 * 
+//	 * @param latestStatus is the new tweet to share
+//	 * 
+//	 */
+//	public static void post(String latestStatus) throws TwitterException {
+//		Twitter twitter = TwitterFactory.getSingleton();
+//		Status status = twitter.updateStatus(latestStatus);
+//		System.out.println("Successfully updated the status to [" + status.getText() + "].");
+//	}
+	
+	/** Testing time line and search for user
+	 * 
+	 *
+	 * 
+	 */
+	public static void main(String[] args) {
+		try {
+			getTimeline();
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		searchForUser("NAMI");
+	}
+}
+    
---
+++ ES1-2018-EIC2-08/src/Tests/GetTimeLine.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.List;
+import twitter4j.Status;
+
+
+public class GetTimeLine {
+	
+	@Test
+	public void test() {
+		TwitterApp ta=new TwitterApp();
+		List<Status> statuses =  ta.statuses;
+//		System.out.println("Showing home timeline.");
+		for (Status status : statuses) {
+//			System.out.println(status.getUser().getName() + ":" + status.getText());
+//			System.out.println("-------------------------");
+			assertNotNull(status);
+		}
+	}
+
+}
