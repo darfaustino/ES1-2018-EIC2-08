ddd7193a3964df2bdd4faa526d7369b986ca4ffb
mspor-iscteiul <mspor@iscte-iul.pt>
2018-11-20 23:20:49 +0000

    GUI UPDATES

---
+++ ES1-2018-EIC2-08/src/BDA/EmailSingular.java
@@ -0,0 +1,43 @@
+package BDA;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+public class EmailSingular {
+	public	JFrame launcher;
+	
+	public EmailSingular(){
+		try {
+			init();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
+
+	public void init() throws IOException {
+		
+		//SettingsJFrame
+		launcher = new JFrame("BOM DIA ACADEMIA!");
+		launcher.setResizable(false);
+		launcher.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		launcher.setLayout(new BorderLayout());
+		launcher.setVisible(true);
+		
+		//Background
+		Background background=new Background("images/emailsingular.png");
+		background.setLayout(new BorderLayout());
+		background.setPreferredSize(new Dimension(800,600));
+		launcher.add(background);
+		launcher.setSize(800, 600);
+		launcher.pack();
+
+	 }
+	public static void main(String args[]){
+	    new EmailSingular();
+
+	}
+	}
