21b4de966a0071e1bf495536f3e4f7695d802f4a
gmide-iscteiul <gmiduarte.13@gmail.com>
2018-11-22 20:58:26 +0000

    Facebook Timeline done

--- ES1-2018-EIC2-08/src/BDA/FacebookTimeline.java
+++ ES1-2018-EIC2-08/src/BDA/FacebookTimeline.java
@@ -1,6 +1,9 @@
 package BDA;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -8,8 +11,10 @@ import java.io.IOException;
 
 public class FacebookTimeline {
 	public	JFrame launcher;
+	private Facebook face;
 	
-	public FacebookTimeline(){
+	public FacebookTimeline(Facebook f){
+		face=f;
 		try {
 			init();
 		} catch (IOException e) {
@@ -23,21 +28,62 @@ public class FacebookTimeline {
 		//SettingsJFrame
 		launcher = new JFrame("BOM DIA ACADEMIA!");
 		launcher.setResizable(false);
-		launcher.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		launcher.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
 		launcher.setLayout(new BorderLayout());
 		launcher.setVisible(true);
 		
 		//Background
 		Background background=new Background("images/facebooktimeline.png");
-		background.setLayout(new BorderLayout());
+		background.setLayout(new GridBagLayout());
 		background.setPreferredSize(new Dimension(800,600));
+
+		GridBagConstraints c=new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=0;
+		c.gridheight=2;
+		c.insets=new Insets(80, 0, 0, 0);
+		
+		JPanel panel=new JPanel();
+		panel.setPreferredSize(new Dimension(730,380));
+		panel.setOpaque(false);
+		background.add(panel, c);
+		
+		JList<FacePost> facePosts=new JList<FacePost>(face.getTimeLinePosts());//filtro retirado pra teste
+		((DefaultListCellRenderer)facePosts.getCellRenderer()).setOpaque(false);
+		facePosts.setFixedCellHeight(70);
+		facePosts.setBorder(new EmptyBorder(10,5, 10, 0));
+		facePosts.setOpaque(false);
+
+		facePosts.addListSelectionListener( new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				
+				if (facePosts.getSelectedValue() != null && !e.getValueIsAdjusting()) {
+					FacePost selectedValue = facePosts.getSelectedValue();
+					new FacebookPost(selectedValue);
+				}
+			}
+		});
+		
+		JScrollPane scroll2=new JScrollPane(facePosts);
+		scroll2.setOpaque(false);
+		scroll2.getViewport().setOpaque(false);
+		scroll2.setPreferredSize(new Dimension(720,375));
+		panel.add(scroll2);
+		
+		
+		JLabel r=new JLabel(new ImageIcon("images/responder.png"));
+		r.setPreferredSize(new Dimension(300,70));
+		c.gridx=0;
+		c.gridy=2;
+		c.insets=new Insets(15, 0, 0, 0);
+		background.add(r,c);
+		
+		
 		launcher.add(background);
 		launcher.setSize(800, 600);
 		launcher.pack();
 
 	 }
-	public static void main(String args[]){
-	    new FacebookTimeline();
-
-	}
-	}
+}
