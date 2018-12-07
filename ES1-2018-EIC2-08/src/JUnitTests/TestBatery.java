a546a9a314c3783043e329cbb03a3ba8049f10c1
mspor-iscteiul <mspor@iscte-iul.pt>
2018-12-07 11:29:24 +0000

    New Test Batery and New ClassDiagram

---
+++ ES1-2018-EIC2-08/src/JUnitTests/TestBatery.java
@@ -0,0 +1,21 @@
+package JUnitTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BackupFPosts.class, BackupMail.class, BackupTweets.class, ContainsFilter_filter.class,
+		DateFilter_filter.class, Face_getExtendedAccessToken.class, Face_getLikedPages.class,
+		Face_getTimeLinePosts.class, Face_getUserInformation.class, FacebookPost_getFacePost.class,
+		FacePost_getDate.class, FacePost_getId.class, FacePost_getMessage.class, FacePost_getPost.class,
+		FacePost_getStory.class, FaceTimeline_getFacebook.class, FetchFromBackup.class, getTextFromMessage.class,
+		getTextFromMimeMultipart.class, InitContainsFilter.class, InitDateFilter.class, InitEmailSingular.class,
+		InitEmailTimeline.class, InitGuiResponse.class, InitHome.class, InitLogin.class, InitMainTimeline.class,
+		InitSignUp.class, InitUserFilter.class, isFacebookOnline.class, isMailOnline.class, isTwitterOnline.class,
+		LoginMail.class, Mail_FetchFromBackup.class, OpenMailForm.class, PostsList.class, SaveXML.class,
+		Tweet_init.class, Twitter_FetchFromBackup.class, Twitter_getTimeLine.class, Twitter_initTwitter.class,
+		Twitter_retweet.class, TwitterTimelineInit.class, UserFilter_filter.class })
+public class TestBatery {
+
+}
